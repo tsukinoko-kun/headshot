2f3a26804370397819f2472e7adf2aa77dfa86e2
Frank Mayer <gh@frankmayer.dev>
2025-04-20 01:26:30 +0200

    fix function

--- generate.go
+++ generate.go
@@ -10,10 +10,17 @@ import (
 )
 
 var (
-	funcRE                   = regexp.MustCompile(`\s*{\n(\s+[^\n]+\n)+}`)
-	oneLineFuncRE            = regexp.MustCompile(`\s*{[^\n]+}`)
-	varRE                    = regexp.MustCompile(`\s*=[^;]+`)
+	funcIgnoreKeywords = []string{"constexpr", "inline"}
+)
+
+var (
+	funcOneLineRE            = regexp.MustCompile(`((?:\w+ +)*[\w:*&<>]+\s+(?:const\s+)?\w+\s*\((?:\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*(?:,\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*)*)?\)(?:\s*const)?)\s*{(?:[^\n]+)}\n`)
+	funcRE                   = regexp.MustCompile(`\n((?:\w+ +)*[\w:*&<>]+\s+(?:const\s+)?\w+\s*\((?:\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*(?:,\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*)*)?\)(?:\s*const)?)\s*{(?:(?:\n {1,}[^\n]+)|\n *)*\n}\n`)
+	func4RE                  = regexp.MustCompile(`\n( {4}(?:\w+ +)*[\w:*&<>]+\s+(?:const\s+)?\w+\s*\((?:\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*(?:,\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*)*)?\)(?:\s*const)?)\s*{(?:(?:\n {5,}[^\n]+)|\n *)*\n {4}}\n`)
+	func8RE                  = regexp.MustCompile(`\n( {8}(?:\w+ +)*[\w:*&<>]+\s+(?:const\s+)?\w+\s*\((?:\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*(?:,\s*(?:const\s*)?[\w:*&<>]+\s+(?:const\s+)?\w+\s*)*)?\)(?:\s*const)?)\s*{(?:(?:\n {9,}[^\n]+)|\n *)*\n {8}}\n`)
+	varRE                    = regexp.MustCompile(`\n[^\s][^\n]+[^=<>]=[^=<>][^\n]+;`)
 	doubleNewlineRE          = regexp.MustCompile(`(\r?\n)(\r?\n)(\r?\n)+`)
+	emptyLineBlockBeginRE    = regexp.MustCompile(`{\n\n`)
 	includeGuardUnderscoreRE = regexp.MustCompile(`[._-]+`)
 )
 
@@ -46,12 +53,52 @@ func generateHeader(name string) {
 		return
 	}
 
-	hpp := funcRE.ReplaceAllString(string(cpp), ";")
-	hpp = oneLineFuncRE.ReplaceAllString(hpp, ";")
+	hpp := string(cpp)
+	hpp = funcOneLineRE.ReplaceAllStringFunc(hpp, func(s string) string {
+		sm := funcOneLineRE.FindSubmatch([]byte(s))
+		funcHead := string(sm[1])
+		for _, kw := range funcIgnoreKeywords {
+			if strings.Contains(funcHead, kw) {
+				return s
+			}
+		}
+		return fmt.Sprintf("%s;\n", funcHead)
+	})
+	hpp = funcRE.ReplaceAllStringFunc(hpp, func(s string) string {
+		sm := funcRE.FindSubmatch([]byte(s))
+		funcHead := string(sm[1])
+		for _, kw := range funcIgnoreKeywords {
+			if strings.Contains(funcHead, kw) {
+				return s
+			}
+		}
+		return fmt.Sprintf("\n%s;\n", funcHead)
+	})
+	hpp = func4RE.ReplaceAllStringFunc(hpp, func(s string) string {
+		sm := func4RE.FindSubmatch([]byte(s))
+		funcHead := string(sm[1])
+		for _, kw := range funcIgnoreKeywords {
+			if strings.Contains(funcHead, kw) {
+				return s
+			}
+		}
+		return fmt.Sprintf("\n%s;\n", funcHead)
+	})
+	hpp = func8RE.ReplaceAllStringFunc(hpp, func(s string) string {
+		sm := func8RE.FindSubmatch([]byte(s))
+		funcHead := string(sm[1])
+		for _, kw := range funcIgnoreKeywords {
+			if strings.Contains(funcHead, kw) {
+				return s
+			}
+		}
+		return fmt.Sprintf("\n%s;\n", funcHead)
+	})
 	hpp = varRE.ReplaceAllString(hpp, "")
 	hpp = selfInclude.ReplaceAllString(hpp, "")
 	hpp = fmt.Sprintf("#ifndef %s\n#define %s\n\n%s\n\n#endif // %s", includeGuard, includeGuard, hpp, includeGuard)
 	hpp = doubleNewlineRE.ReplaceAllString(hpp, "\n\n")
+	hpp = emptyLineBlockBeginRE.ReplaceAllString(hpp, "{\n")
 
 	hppF, err := os.Create(hppName)
 	if err != nil {
